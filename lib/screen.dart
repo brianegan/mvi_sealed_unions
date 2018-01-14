ea5876364cdd4346757affe89beca86806a81f4a
Brian Egan <brian@brianegan.com>
2018-01-14 19:12:13 +0100

    Fade those images in

--- lib/screen.dart
+++ lib/screen.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:typed_data';
 
 import 'package:dribbble_client/dribbble_client.dart';
 import 'package:flutter/foundation.dart';
@@ -141,8 +142,10 @@ class ShotItem extends StatelessWidget {
           child: new AspectRatio(
             aspectRatio: 4 / 3,
             child: new GestureDetector(
-              child: new Image.network(
-                shot.images.teaser,
+              child: new FadeInImage.memoryNetwork(
+                fadeInDuration: new Duration(milliseconds: 200),
+                placeholder: kTransparentImage,
+                image: shot.images.teaser,
                 fit: BoxFit.cover,
               ),
               onTap: () {
@@ -214,3 +217,70 @@ class LoadingItem extends StatelessWidget {
     );
   }
 }
+
+final Uint8List kTransparentImage = new Uint8List.fromList(<int>[
+  0x89,
+  0x50,
+  0x4E,
+  0x47,
+  0x0D,
+  0x0A,
+  0x1A,
+  0x0A,
+  0x00,
+  0x00,
+  0x00,
+  0x0D,
+  0x49,
+  0x48,
+  0x44,
+  0x52,
+  0x00,
+  0x00,
+  0x00,
+  0x01,
+  0x00,
+  0x00,
+  0x00,
+  0x01,
+  0x08,
+  0x06,
+  0x00,
+  0x00,
+  0x00,
+  0x1F,
+  0x15,
+  0xC4,
+  0x89,
+  0x00,
+  0x00,
+  0x00,
+  0x0A,
+  0x49,
+  0x44,
+  0x41,
+  0x54,
+  0x78,
+  0x9C,
+  0x63,
+  0x00,
+  0x01,
+  0x00,
+  0x00,
+  0x05,
+  0x00,
+  0x01,
+  0x0D,
+  0x0A,
+  0x2D,
+  0xB4,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x49,
+  0x45,
+  0x4E,
+  0x44,
+  0xAE,
+]);
